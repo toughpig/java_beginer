fb2e100bf116d269d3075e1fddd7772ea3f1eeb2
WangHao <378981240@qq.com>
2016-07-18 07:47:22 +0800

    Update List_test.java

--- List_test.java
+++ List_test.java
@@ -1,11 +1,8 @@
 package List;
 
-import java.awt.Frame;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 @SuppressWarnings("serial")
 public class List_test extends Frame implements ActionListener{
